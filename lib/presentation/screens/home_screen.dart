b47fa0001e5d5621d5b5fe63015d5c492d6736c2
david <datskodavid@gmail.com>
2023-08-16 17:40:45 +0300

    add person details screen

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tesk_task_getx/data/remote_datasource.dart';
+import 'package:tesk_task_getx/presentation/screens/person_details_screen.dart';
 import 'package:tesk_task_getx/presentation/widgets/person_card.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,7 +10,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Test task'),
+        title: const Text('Home'),
       ),
       body: FutureBuilder(
         future: RemoteDatsource.getData(),
@@ -25,20 +26,36 @@ class HomeScreen extends StatelessWidget {
             );
           }
 
+          final personData = snapshot.data!.data;
+
           return ListView.separated(
             itemCount: 6,
             separatorBuilder: (context, index) {
               return const Padding(
-                padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 10),
+                padding: EdgeInsets.symmetric(vertical: 3.0, horizontal: 10),
                 child: Divider(),
               );
             },
             itemBuilder: (context, index) {
               return PersonCard(
-                avatar: snapshot.data!.data[index].avatar,
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PersonDetailsScreen(
+                        id: personData[index].id.toInt(),
+                        avatar: personData[index].avatar,
+                        name:
+                            '${personData[index].firstName} ${personData[index].lastName}',
+                        email: personData[index].email,
+                      ),
+                    ),
+                  );
+                },
+                avatar: personData[index].avatar,
                 name:
-                    '${snapshot.data!.data[index].firstName} ${snapshot.data!.data[index].lastName}',
-                email: snapshot.data!.data[index].email,
+                    '${personData[index].firstName} ${personData[index].lastName}',
+                email: personData[index].email,
               );
             },
           );
---
+++ lib/presentation/screens/person_details_screen.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class PersonDetailsScreen extends StatelessWidget {
+  final int id;
+  final String avatar;
+  final String name;
+  final String email;
+
+  const PersonDetailsScreen({
+    Key? key,
+    required this.id,
+    required this.avatar,
+    required this.name,
+    required this.email,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Person Details'),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(height: 40),
+                CircleAvatar(
+                  radius: 60,
+                  foregroundImage: NetworkImage(avatar),
+                ),
+                const SizedBox(height: 30),
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  margin: EdgeInsets.symmetric(horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          const Text(
+                            'ID:',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          const SizedBox(width: 53),
+                          Text(
+                            id.toString(),
+                            style: const TextStyle(fontSize: 18),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          const Text(
+                            'Email:',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          const SizedBox(width: 20),
+                          Text(
+                            email,
+                            style: const TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+}
